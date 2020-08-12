52c5787c9d56be0469131ab8a83007f6021f5d77
snaiya <naiya0873@gmail.com>
2020-08-11 20:08:09 -0700

    Updates

---
+++ precache-manifest.920f45cb5745fcd9783dfb51a0438c6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8557d018b04e5e915bb2953e4d5b1a8b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "bb5e9b8808d7251a5f71",
+    "url": "/Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e894dbc2617d0ef5c596",
+    "url": "/Portfolio/static/css/main.36744c77.chunk.css"
+  },
+  {
+    "revision": "bb5e9b8808d7251a5f71",
+    "url": "/Portfolio/static/js/2.f56fdbd0.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.f56fdbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e894dbc2617d0ef5c596",
+    "url": "/Portfolio/static/js/main.25b170e6.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "6ed601af3b2cacca330992c9db6cd5f9",
+    "url": "/Portfolio/static/media/N_Resume_CS.6ed601af.pdf"
+  },
+  {
+    "revision": "5aea130462220ffbb6ca8800057a798e",
+    "url": "/Portfolio/static/media/user.5aea1304.jpeg"
+  }
+]);
